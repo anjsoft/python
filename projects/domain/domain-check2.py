ff6173c6067dc424706fc270808b696ee5d7ac52
anjun <anjsoft@gmail.com>
2014-11-13 23:48:47 +0800

    a

--- projects/domain/domain-check2.py
+++ projects/domain/domain-check2.py
@@ -1,9 +1,10 @@
 from bs4 import BeautifulSoup
 import urllib
+from pyquery import PyQuery as pyq
 import time
 namelist=[]
 def readName():
-    with open('five-num-dict.txt','r') as f:
+    with open('3-num-dict.txt','r') as f:
         for line in f:
              namelist.append(line[:-1]+'.com')
         # print(namelist)
@@ -14,8 +15,13 @@ def save( name):
 
 def check(name):
     text = urllib.urlopen("http://whois.chinaz.com/"+name)
-    print text.read()
+    # print text.read()
+    doc=  pyq(text.read())
+
+    cts= doc('.div_whois')
+    print cts.text()
     exit(0)
+
     # text = text.read().decode("gb2312").encode("utf-8")
 	# text = text.replace(" encoding=\"gb2312\"?>"," encoding=\"utf-8\"?>")
 	# print text
@@ -31,4 +37,4 @@ if __name__ == '__main__':
     # readName()
     # for name in namelist:
     #     check(name)
-    check("anjsoft.com")
+    check("anjsoftfff.com")
--- projects/domain/whois-check.sh
+++ projects/domain/whois-check.sh
@@ -1,7 +1,8 @@
 echo 'start'
-for name in $(cat 2-nums-dict.txt)
+for name in $(cat 4-nums-dict.txt)
 do
 echo $name.com
-sleep 5
+sleep 1
 whois $name.com|cat|grep 'No match'>>ok-domain.txt
+sed -i -e "1d" 4-nums-dict.txt
 done 
